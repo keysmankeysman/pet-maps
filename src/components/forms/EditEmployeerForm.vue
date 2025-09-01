387d6f9c06d3d3af046573018846939e5b0b1c50
unknown <dima>
2025-09-01 22:59:57 +0300

    some

--- src/components/forms/EditEmployeerForm.vue
+++ src/components/forms/EditEmployeerForm.vue
@@ -19,15 +19,21 @@ watch(formData, (newVal) => {
 
 
 const addEmployeer = () => {
-  console.log('formData.value 2', formData.value)
   formData.value.push({
-    id: 20,
+    id: formData.value.length + 1,
     firstName: '',
     lastName: '',
     middleName: '',
     phone: '',
     shopId: 15,
   })
+}
+
+const delEmployeer = (id) => {
+  console.log('delEmployeer', id)
+  const index = formData.value.findIndex(el => el.id === id)
+  console.log('index', index)
+  formData.value.splice(index, 1)
 }
 
 // const addEmployeer = () => {
@@ -47,28 +53,43 @@ const addEmployeer = () => {
 <template>
   <v-sheet class="mx-auto" width="600">
     <v-form v-if="formData.length" fast-fail @submit.prevent>
-      <div v-for="formData in formData" :key="formData.id">
+      <div v-for="employeer in formData" :key="employeer.id">
         <v-row>
           <v-text-field
             class="mr-4"
-            v-model="formData.firstName"
+            v-model="employeer.firstName"
             label="Имя"
           ></v-text-field>
           <v-text-field
             class="mr-4"
-            v-model="formData.lastName"
+            v-model="employeer.lastName"
             label="Фамилия"
           ></v-text-field>
           <v-text-field
-            v-model="formData.middleName"
+            v-model="employeer.middleName"
             label="Отчество"
           ></v-text-field>
         </v-row>
-        <v-text-field
-          v-model="formData.phone"
-          label="Номер телефона"
-        ></v-text-field>
+        <v-row>
+          <v-text-field
+            v-model="employeer.phone"
+            label="Номер телефона"
+          ></v-text-field>
+          <v-btn
+            class="ms-auto"
+            color="error"
+            @click="delEmployeer(employeer.id)"
+            text="удалить"
+          ></v-btn>
+        </v-row>
+
+
       </div>
+      <v-btn
+        class="ms-auto"
+        @click="addEmployeer"
+        text="Добавить"
+      ></v-btn>
     </v-form>
     <div v-else>
       Нет сотрудников
